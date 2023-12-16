40315aa12695bb8463b7316a1ae449dcf4224208
许乐 <xule@ikingtec.com>
2023-12-16 16:01:18 +0800

    安装element

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import HelloWorld from './views/HelloWorld.vue'
-import ProgressRing_1 from "./views/ProgressRing_1.vue"
-import ProgressRing_2 from "./views/ProgressRing_2.vue"
+// import HelloWorld from './views/HelloWorld.vue'
+// import ProgressRing_1 from "./views/ProgressRing_1.vue"
+// import ProgressRing_2 from "./views/ProgressRing_2.vue"
+import StudyTool from "./views/StudyTool/StudyTool.vue"
 </script>
 
 <template>
-  <HelloWorld />
-  <ProgressRing_1 />
-  <ProgressRing_2 />
+  <StudyTool />
 </template>
---
+++ src/views/StudyTool/StudyTool.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<template>
+  <el-button type="primary">Primary</el-button>
+</template>
+
+<style scoped></style>
