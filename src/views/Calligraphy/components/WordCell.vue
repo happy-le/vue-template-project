b5fbfb1a1b96439b893035b0ccc39c5543bd12f2
happy-le <1113632076@qq.com>
2024-05-22 16:35:55 +0800

    feat:单元格样式修改

--- src/views/Calligraphy/components/WordCell.vue
+++ src/views/Calligraphy/components/WordCell.vue
@@ -17,29 +17,31 @@ export default {
 <style lang="scss" scoped>
 @font-face {
   font-family: JXP;
-  src: url(../JXP-.ttf)  format('truetype');
+  src: url(../JXP-.ttf) format('truetype');
 }
 
 .word_cell_main_content {
   --cell_color: rgba(255, 0, 0, 0.6);
+  --cell-width: 145px;
+  --cell-border-width: 4px;
 
   position: relative;
-  width: 50px;
-  height: 50px;
+  width: var(--cell-width);
+  height: var(--cell-width);
   overflow: hidden;
   border-style: solid;
   border-color: var(--cell_color);
   border-top-width: 0px;
   border-right-width: 0px;
-  border-bottom-width: 2px;
-  border-left-width: 2px;
+  border-bottom-width: var(--cell-border-width);
+  border-left-width: var(--cell-border-width);
 
   display: flex;
   align-items: center;
   justify-content: center;
 
   span {
-    font-size: 35px;
+    font-size: 100px;
     font-family: JXP;
     font-weight: bold;
     position: relative;
@@ -47,38 +49,36 @@ export default {
     opacity: 0.8;
   }
 
-  &:nth-child(4n) {
-    border-right-width: 2px;
+  &:nth-child(9n) {
+    border-right-width: var(--cell-border-width);
   }
 
-  &:nth-child(-n + 4) {
-    border-top-width: 2px;
+  &:nth-child(-n + 9) {
+    border-top-width: var(--cell-border-width);
   }
 
-  &::before {
+  &::before,
+  &::after {
     content: '';
     position: absolute;
     top: 0;
-    left: 0;
     z-index: -1;
 
-    width: 75px;
+    width: calc(var(--cell-width) * 2);
     height: 0px;
-    border-bottom: 1px var(--cell_color) dashed;
+    border-style: dashed;
+    border-color: var(--cell_color);
+    border-width: 0 0 var(--cell-border-width) 0;
+  }
+
+  &::before {
+    left: 0;
     transform: rotate(45deg);
     transform-origin: 0 0;
   }
 
   &::after {
-    content: '';
-    position: absolute;
-    top: 0;
     right: 0;
-    z-index: -1;
-
-    width: 75px;
-    height: 0px;
-    border-bottom: 1px var(--cell_color) dashed;
     transform: rotate(-45deg);
     transform-origin: 100% 0;
   }
--- src/views/Calligraphy/indexPage.vue
+++ src/views/Calligraphy/indexPage.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="word_content">
-  <WordCell v-for="(item, index) in strArr" :key="index">
-    {{ item }}
-  </WordCell>
-</div>
+    <WordCell v-for="(item, index) in strArr" :key="index">
+      {{ item }}
+    </WordCell>
+  </div>
 </template>
 
 <script>
@@ -14,24 +14,33 @@ export default {
   },
   data() {
     return {
+      pageLen: 98, // 每一页字数个数
       str: '见多识广察言观色高瞻远瞩'
     }
   },
 
   computed: {
     strArr: function () {
-      return this.str.split('')
+      return this.str.split('').concat(new Array(86))
     }
   },
+
   methods: {}
 }
 </script>
+
 <style lang="scss" scoped>
 .word_content {
-  width: 250px;
+  width: 1440px;
+  height: 2080px;
   display: flex;
+  align-content: flex-start;
   flex-wrap: wrap;
+
+  background-color: rgba(218, 255, 196, 0.616);
+  padding: 150px 0px 0px 300px;
+
+  transform: scale(0.3, 0.3);
+  transform-origin: 0 0;
 }
-
 </style>
-
